4d589fbd86667c158c71c41d95b0efcf1b07e76d
Badr <badrkmc@gmail.com>
2022-11-08 19:33:26 +0500

    update dataset

--- Week01/dataset.py
+++ Week01/dataset.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv("iris.data")
+dataset = pd.read_csv("iris.csv")
 x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
+y = dataset.iloc[:, 3].values
 
 print(x)
 print(y)
